f54f9b87397c4dfd22985c5ee9ddac527c744233
eeedubs <ericswilson94@gmail.com>
2018-12-06 07:16:56 +0000

    User can now add categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,24 +1,27 @@
 class Admin::CategoriesController < ApplicationController
+    def index
+        @categories = Category.order(id: :desc).all
+    end
+    
     def new
-        @category = category.new
+        @category = Category.new
     end
 
     def create
-        @category = category.new(category_params)
+        @category = Category.new(category_params)
 
         if @category.save
             redirect_to [:admin, :categories], notice: 'Category created!'
         else
             render :new
+        end
     end
     
     private
 
-    def product_params
+    def category_params
       params.require(:category).permit(
         :name
-        # :description,
-        # :category_id
       )
     end
     
