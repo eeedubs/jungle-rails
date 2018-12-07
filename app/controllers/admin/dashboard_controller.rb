87b77d61c349c24617c41a0a0a6b2c90fa0fc5cd
eeedubs <ericswilson94@gmail.com>
2018-12-07 08:32:34 +0000

    Changes to login cookies

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: 'Jungle', password: 'book', except: :not_permitted
+
+  def not_permitted
+      render plain: "You do not have access to these features."
+  end
+
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,9 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: 'Jungle', password: 'book', except: :not_permitted
+
+  def not_permitted
+      render plain: "You do not have access to these features."
+  end
 
   def index
     @products = Product.order(id: :desc).all
