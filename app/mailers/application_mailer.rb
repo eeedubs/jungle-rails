82e5bd36a3e0be775c714ba859343dae963cbc15
eeedubs <ericswilson94@gmail.com>
2018-12-07 08:13:14 +0000

    Email now returns in proper format

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,8 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-<<<<<<< HEAD
   default from: "no-reply@jungle.com"
-=======
-  default from: "from@example.com"
->>>>>>> 6a942e81ffda9a987f50cd73ff23743b41d15591
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,4 @@
 class UserMailer < ApplicationMailer
-<<<<<<< HEAD
 default from: "no-reply@jungle.com"
 
     def order_email(email, order)
@@ -7,14 +6,4 @@ default from: "no-reply@jungle.com"
         @order = order
         mail(to: email, subject: "Order Confirmation ID: #{@order.id}")
     end
-=======
-    default from 'no-reply@jungle.com'
-
-    def order_email
-        @user = params[:user]
-        @url = 'http://localhost:3000/orders/:id'
-        mail(to: @order.email, subject: 'Confirmation of Order ID: <%= @order.id %>')
-    end
-
->>>>>>> 6a942e81ffda9a987f50cd73ff23743b41d15591
 end
