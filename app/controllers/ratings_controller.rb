b25fb62d15cd14a91fd80668f3b168b9526f3f88
eeedubs <ericswilson94@gmail.com>
2018-12-08 06:49:00 +0000

    some small css fixes

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -23,6 +23,8 @@ class RatingsController < ApplicationController
         redirect_to :back
     end
 
+    (Rating.where(:product_id => @product.id).sum(:rating) / Rating.where(:product_id => @product.id).length)/1.0
+
     def check
         unless current_user
             redirect_to '/'
